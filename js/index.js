8eb346607be2ed3fa3381932472ec7ffdea873e3
Ivan Castillo <ivancastillo.1996@gmail.com>
2021-05-06 11:49:57 -0600

    update

--- js/index.js
+++ js/index.js
@@ -1,18 +1,33 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        totalBill: 0,
-        totalConsum: 0,
-        sssn:0
+  el: "#app",
+  data: {
+    totalBill: 0,
+    totalConsum: 0,
+    sssn: 0,
+    rate: 0.14,
+    capacity: 34.5,
+  },
+  methods: {
+    key: function (num) {
+      return (this.total += num);
     },
-    methods: {
-        key: function(num) {
-        return this.total += num;
-        },
-        equal: function() {
-         let equal = this.total;
-         return this.total = eval(equal);
-        }         
-    }
+    totalToBill: function () {
+      this.totalConsum = 0;
+      let kws = 0;
+      if (this.totalBill > 0) {
+        kws = parseFloat(this.totalBill) / parseFloat(this.rate);
+        this.totalConsum = kws;
+        this.sssn = (kws / this.capacity) * 340;
+      }
+    },
+    totalToConsum: function () {
+        this.totalBill=0;
+      let kws = 0;
+      if (this.totalConsum > 0) {
+        kws = this.totalConsum;
+        this.totalBill = parseFloat(this.totalConsum) * this.rate;
+        this.sssn = (kws / this.capacity) * 340;
+      }
+    },
+  },
 });
-  
