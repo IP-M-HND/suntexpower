9b9e3db5de0c6ac8d049a7004807a9c634d63b7c
Ivan Peña <ivancastillo.1996@gmail.com>
2022-08-15 09:33:02 -0600

    correcciones pantalla quiz

--- js/index.js
+++ js/index.js
@@ -21,6 +21,16 @@ function div1(yes) {
 
 function div2() {
 
+    if ($("form#contact-form input[name='message']").val() === null || $("form#contact-form input[name='message']").val() === '') {
+        return false;
+    }
+    if ($("form#contact-form input[name='zipCode']").val() === null || $("form#contact-form input[name='zipCode']").val() === '') {
+        return false;
+    }
+    if ($("form#contact-form input[name='city']").val() === null || $("form#contact-form input[name='city']").val() === '') {
+        return false;
+    }
+
     var element = document.getElementById("progressbar");
     element.classList.add("w-50");
     var element2 = document.getElementById("progressbar");
@@ -43,5 +53,31 @@ function div3() {
     element.classList.add("d-none");
 
     var element2 = document.getElementById("div4");
+    element2.classList.remove("d-none");
+}
+
+
+function div4() {
+    if ($("form#contact-form input[name='firstName']").val() === null || $("form#contact-form input[name='firstName']").val() === '') {
+        return false;
+    }
+    if ($("form#contact-form input[name='lastName']").val() === null || $("form#contact-form input[name='lastName']").val() === '') {
+        return false;
+    }
+    if ($("form#contact-form input[name='phone']").val() === null || $("form#contact-form input[name='phone']").val() === '') {
+        return false;
+    }
+    if ($("form#contact-form input[name='email']").val() === null || $("form#contact-form input[name='email']").val() === '') {
+        return false;
+    }
+    var element = document.getElementById("progressbar");
+    element.classList.add("w-100");
+    var element2 = document.getElementById("progressbar");
+    element2.classList.remove("w-75");
+
+    var element = document.getElementById("div4");
+    element.classList.add("d-none");
+
+    var element2 = document.getElementById("div5");
     element2.classList.remove("d-none");
 }
